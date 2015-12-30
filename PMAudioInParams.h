fafc68d64037a8efef37f6b0beac151033f23bc8
Miquel Soler <miquelangelsoler@gmail.com>
2015-12-30 01:01:10 +0100

    Squashed 'PedreraAudioAnalyzerSender/src/External/PMAudioAnalysis/' content from commit 76a316a
    git-subtree-dir: PedreraAudioAnalyzerSender/src/External/PMAudioAnalysis
    git-subtree-split: 76a316aa175aaf199ae602f5d68622162078e5bf

---
+++ PMAudioInParams.h
@@ -0,0 +1,55 @@
+//
+//  PMAudioOutParams.h
+//  PMConcertParaules
+//
+//  Created by Miquel Àngel Soler on 13/10/15.
+//
+
+#ifndef PMAudioOutParams_h
+#define PMAudioOutParams_h
+
+#pragma once
+
+struct baseAudioInParams {
+    unsigned int audioInputIndex;
+    int deviceID;
+};
+
+struct pitchParams : public baseAudioInParams {
+    float midiNote;
+};
+
+struct energyParams : public baseAudioInParams {
+    float energy;
+};
+
+struct silenceParams : public baseAudioInParams {
+    bool isSilent;
+    float silenceTime;
+};
+
+struct freqBandsParams : public baseAudioInParams {
+    float *melBands;
+    int numBands;
+};
+
+struct onsetParams : public baseAudioInParams {
+    bool isOnset;
+};
+
+struct shtParams : public baseAudioInParams {
+    bool isSht;
+    float time;
+};
+
+struct pauseParams : public baseAudioInParams {
+    bool isPaused;
+    float pauseTime;
+};
+
+struct melodyDirectionParams : public baseAudioInParams {
+    float direction;
+};
+
+
+#endif /* PMAudioOutParams_h */
