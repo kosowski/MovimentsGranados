36e2fa10b46c3bf988ad220a8defb08378b7cdbe
Miquel Soler <miquelangelsoler@gmail.com>
2016-01-04 03:25:01 +0100

    Added master OSC receiver port to OSCSettings.h

--- Shared/OSCSettings.h
+++ Shared/OSCSettings.h
@@ -11,6 +11,9 @@
 #ifndef MovimentsGranados_OSCSettings_h
 #define MovimentsGranados_OSCSettings_h
 
+// Master
+static const int			OSC_MASTER_RECEIVER_PORT	= 30300;
+
 // Cello analyzer
 static const string         OSC_CELLO_SENDER_HOST		= "localhost";
 static const int            OSC_CELLO_SENDER_PORT		= 30301;
