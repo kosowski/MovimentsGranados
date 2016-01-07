f8d7b83bdb9aad5b6d6c92eda946861f69a8868b
Miquel Soler <miquelangelsoler@gmail.com>
2016-01-07 10:01:48 +0100

    Added GUI to KinectSender.

--- PedreraKinectSender/src/ofApp.cpp
+++ PedreraKinectSender/src/ofApp.cpp
@@ -1,71 +1,122 @@
 #include "ofApp.h"
+#include "../../Shared/OSCSettings.h"
 
-//--------------------------------------------------------------
-void ofApp::setup(){
+static const string STR_APP_TITLE = "KINECT DETECTOR";
+static const string STR_GUI_TITLE = "Kinect Status";
 
+static const string SETTINGS_FILENAME = "settings.xml";
+
+static const string STR_STATE               = "STATUS";
+static const string STR_STATE_SETUP         = "Initializing Kinect...";
+static const string STR_STATE_DETECTING     = "Detecting Person...";
+static const string STR_STATE_CAPTURING     = "Capturing";
+static const string STR_BUTTON_RESTART      = "RESTART";
+
+static const int GUI_POSX = 10;
+static const int GUI_POSY = 10;
+static const int GUI_WIDTH  = 350;
+
+void ofApp::setup()
+{
+    ofSetWindowTitle(STR_APP_TITLE);
+    ofBackground(ofColor::black);
+
+    currState = prevState = STATE_SETUP;
+
+    // GUI
+    {
+        gui.setup(STR_GUI_TITLE, SETTINGS_FILENAME);
+        gui.setPosition(GUI_POSX, GUI_POSY);
+        gui.loadFromFile(SETTINGS_FILENAME);
+
+        gui.add(guiStatusLbl.setup(STR_STATE, STR_STATE_SETUP));
+        guiStatusLbl.setBackgroundColor(ofColor::darkRed);
+        guiStatusLbl.setDefaultWidth(GUI_WIDTH);
+        gui.add(guiRestartBtn.setup(STR_BUTTON_RESTART));
+
+        gui.setSize(GUI_WIDTH, GUI_WIDTH);
+        gui.setWidthElements(GUI_WIDTH);
+    }
+
+    // OSC
+    {
+        oscSender.setup(OSC_KINECT_SENDER_HOST, OSC_KINECT_SENDER_PORT);
+    }
 }
 
-//--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::update()
+{
+    if (currState != prevState)
+    {
+        // Update state label
+        string stateDescr;
+        switch(currState)
+        {
+            case STATE_SETUP:
+                stateDescr = STR_STATE_SETUP;
+                guiStatusLbl.setBackgroundColor(ofColor::darkRed);
+                break;
+            case STATE_DETECTING:
+                stateDescr = STR_STATE_DETECTING;
+                guiStatusLbl.setBackgroundColor(ofColor::darkBlue);
+                break;
+            case STATE_CAPTURING:
+                stateDescr = STR_STATE_CAPTURING;
+                guiStatusLbl.setBackgroundColor(ofColor::darkGreen);
+                break;
+        }
+        guiStatusLbl.setup(STR_STATE, stateDescr);
 
+        prevState = currState;
+    }
+
+    switch(currState)
+    {
+        case STATE_SETUP:
+        {
+            break;
+        }
+        case STATE_DETECTING:
+        {
+            break;
+        }
+        case STATE_CAPTURING:
+        {
+            break;
+        }
+    }
 }
 
-//--------------------------------------------------------------
-void ofApp::draw(){
-
+void ofApp::draw()
+{
+    gui.draw();
 }
 
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-
+void ofApp::exit()
+{
+    gui.saveToFile(SETTINGS_FILENAME);
 }
 
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
+void ofApp::keyReleased(int key)
+{
+    // TODO: Remove after Kinect is working. The code below is just to simulate state changes.
+    switch(key)
+    {
+        case '1': currState = STATE_SETUP; break;
+        case '2': currState = STATE_DETECTING; break;
+        case '3': currState = STATE_CAPTURING; break;
+        default: break;
+    }
 }
 
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
+void ofApp::mouseDragged(int x, int y, int button)
+{
 }
 
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
+void ofApp::mousePressed(int x, int y, int button)
+{
 }
 
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+void ofApp::mouseReleased(int x, int y, int button)
+{
 }
--- PedreraKinectSender/src/ofApp.h
+++ PedreraKinectSender/src/ofApp.h
@@ -1,24 +1,37 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxGUI.h"
+#include "ofxOsc.h"
 
-class ofApp : public ofBaseApp{
+typedef enum
+{
+    STATE_SETUP = 0,
+    STATE_DETECTING = 1,
+    STATE_CAPTURING = 2
+} KinectSenderState;
 
-	public:
-		void setup();
-		void update();
-		void draw();
+class ofApp : public ofBaseApp
+{
+public:
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+    void setup();
+    void update();
+    void draw();
+    void exit();
+
+    void keyReleased(int key);
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+
+private:
+
+    KinectSenderState currState, prevState;
+
+    ofxPanel gui;
+    ofxLabel guiStatusLbl;
+    ofxButton guiRestartBtn;
+
+    ofxOscSender oscSender;
 };
