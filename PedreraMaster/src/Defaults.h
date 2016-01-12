356c810cdbe8f808b90b0049324435801f231346
Miquel Soler <miquelangelsoler@gmail.com>
2016-01-12 16:22:19 +0100

    Renamed default window parameters.

--- PedreraMaster/src/Defaults.h
+++ PedreraMaster/src/Defaults.h
@@ -9,9 +9,14 @@
 #ifndef ConductrEnter_Config_h
 #define ConductrEnter_Config_h
 
-static const int            DEFAULT_WINDOW_WIDTH        = 1280;
-static const int            DEFAULT_WINDOW_HEIGHT       = 1024;
+static const int            MAIN_WINDOW_WIDTH = 1280;
+static const int            MAIN_WINDOW_HEIGHT = 1024;
+static const bool           MAIN_WINDOW_RESIZABLE = false;
+static const ofWindowMode   MAIN_WINDOW_MODE = OF_WINDOW;
 
-static const ofWindowMode   DEFAULT_WINDOW_MODE         = OF_WINDOW;
+static const int            GUI_WINDOW_WIDTH = 1280;
+static const int            GUI_WINDOW_HEIGHT = 1024;
+static const bool           GUI_WINDOW_RESIZABLE = true;
+static const ofWindowMode   GUI_WINDOW_MODE = OF_WINDOW;
 
 #endif
