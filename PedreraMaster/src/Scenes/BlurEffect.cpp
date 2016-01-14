bd71fda0dbab764e083771975b11eda5d4905bd8
nacho cossio <info@nachocossio.com>
2016-01-14 17:55:04 +0100

    New class for applying a 2 pass gaussian blur effect

---
+++ PedreraMaster/src/Scenes/BlurEffect.cpp
@@ -0,0 +1,111 @@
+	//
+//  BlurEffect.cpp
+//  PedreraMaster
+//
+//  Created by nacho on 14/01/16.
+//
+//
+
+#include "BlurEffect.h"
+
+BlurEffect::BlurEffect() {
+    initialized = false;
+}
+
+void BlurEffect::setup(float w, float h) {
+    
+    firstPass.allocate(w,h);
+    
+    firstPass.begin();
+    ofClear(0, 0, 0, 0);
+    firstPass.end();
+  
+    string vertexShader =
+    "void main() {\
+    gl_TexCoord[0] = gl_MultiTexCoord0;\
+    gl_Position = ftransform();\
+    }";
+    
+    //what the blur shader does: it takes blurAmnt as input and takes for each 'pixel' / fragment a
+    //weighted average of itself and 8 horizontal neighbours (which neighbours depends on the blurAmnt
+    
+    string fragmentShaderHorizontal =
+    "uniform sampler2DRect src_tex_unit0;\
+    uniform float blurAmount;\
+    void main(void) {\
+    vec2 st = gl_TexCoord[0].st;\
+    vec4 color=vec4(0.);\
+    color += 1.0 * texture2DRect(src_tex_unit0, st + vec2(blurAmount * -4.0, 0.0));\
+    color += 2.0 * texture2DRect(src_tex_unit0, st + vec2(blurAmount * -3.0, 0.0));\
+    color += 3.0 * texture2DRect(src_tex_unit0, st + vec2(blurAmount * -2.0, 0.0));\
+    color += 4.0 * texture2DRect(src_tex_unit0, st + vec2(blurAmount * -1.0, 0.0));\
+    color += 5.0 * texture2DRect(src_tex_unit0, st );\
+    color += 4.0 * texture2DRect(src_tex_unit0, st + vec2(blurAmount * 1.0, 0.0));\
+    color += 3.0 * texture2DRect(src_tex_unit0, st + vec2(blurAmount * 2.0, 0.0));\
+    color += 2.0 * texture2DRect(src_tex_unit0, st + vec2(blurAmount * 3.0, 0.0));\
+    color += 1.0 * texture2DRect(src_tex_unit0, st + vec2(blurAmount * 4.0, 0.0));\
+    color /= 25.0;\
+    gl_FragColor = color;\
+    }";
+    
+    // same but now for vertical neighbours
+    
+    string fragmentShaderVertical =
+    "uniform sampler2DRect src_tex_unit0;\
+    uniform float blurAmount;\
+    void main(void) {\
+    vec2 st = gl_TexCoord[0].st;\
+    vec4 color=vec4(0.);\
+    color += 1.0 * texture2DRect(src_tex_unit0, st + vec2(0.0, blurAmount * 4.0));\
+    color += 2.0 * texture2DRect(src_tex_unit0, st + vec2(0.0, blurAmount * 3.0));\
+    color += 3.0 * texture2DRect(src_tex_unit0, st + vec2(0.0, blurAmount * 2.0));\
+    color += 4.0 * texture2DRect(src_tex_unit0, st + vec2(0.0, blurAmount * 1.0));\
+    color += 5.0 * texture2DRect(src_tex_unit0, st );\
+    color += 4.0 * texture2DRect(src_tex_unit0, st + vec2(0.0, blurAmount * -1.0));\
+    color += 3.0 * texture2DRect(src_tex_unit0, st + vec2(0.0, blurAmount * -2.0));\
+    color += 2.0 * texture2DRect(src_tex_unit0, st + vec2(0.0, blurAmount * -3.0));\
+    color += 1.0 * texture2DRect(src_tex_unit0, st + vec2(0.0, blurAmount * -4.0));\
+    color /= 25.0;\
+    gl_FragColor = color;\
+    }";
+    
+    
+    vShader.setupShaderFromSource(GL_FRAGMENT_SHADER, fragmentShaderVertical);
+    vShader.setupShaderFromSource(GL_VERTEX_SHADER, vertexShader);
+    vShader.linkProgram();
+    
+    hShader.setupShaderFromSource(GL_FRAGMENT_SHADER, fragmentShaderHorizontal);
+    hShader.setupShaderFromSource(GL_VERTEX_SHADER, vertexShader);
+    hShader.linkProgram();
+    
+    initialized = true;
+}
+
+void BlurEffect::apply(ofFbo *targetFbo, int amount, int iterations) {
+    if (!initialized) ofLog(OF_LOG_ERROR, "ofxBlurShader::setup(w,h) needs to be called first");
+    
+    this->amount = amount;
+    this->iterations = iterations;
+    
+    
+    for (int i=0; i<iterations; i++) {
+        
+        //apply horizontal blur to fbo1 and put the result in fbo2
+        firstPass.begin();
+        hShader.begin();
+        hShader.setUniform1f("blurAmount", amount);
+        targetFbo->draw(0,0);
+        hShader.end();
+        firstPass.end();
+        
+        //apply vertical blur to fbo2 and put this back in fbo1
+        targetFbo->begin();
+        vShader.begin();
+        vShader.setUniform1f("blurAmount", amount);
+        firstPass.draw(0,0);
+        hShader.end();
+        targetFbo->end();
+    }
+}
+
+
---
+++ PedreraMaster/src/Scenes/BlurEffect.h
@@ -0,0 +1,28 @@
+//
+//  BlurEffect.h
+//  PedreraMaster
+//
+//  Created by nacho on 14/01/16.
+//
+//
+
+#ifndef BlurEffect_h
+#define BlurEffect_h
+
+#include "ofMain.h"
+class BlurEffect{
+public:
+    
+    BlurEffect();
+    void setup(float w, float h);
+    void apply(ofFbo *targetFbo, int amount, int iterations);
+    
+    int iterations, amount;
+    bool initialized;
+    
+    ofFbo firstPass;
+    ofShader hShader, vShader;
+
+};
+
+#endif /* BlurEffect_h */
