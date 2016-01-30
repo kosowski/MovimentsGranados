39b6e1b64ce9f47a0a9861fa3a30eb49840e1593
eduardfrigola <eduardfrigola@yahoo.com>
2016-01-30 14:00:44 +0100

    Working on kinectReceiver

--- PedreraKinectReceiver/src/ofApp.cpp
+++ PedreraKinectReceiver/src/ofApp.cpp
@@ -1,15 +1,11 @@
 #include "ofApp.h"
+#include "../../Shared/OSCSettings.h"
 
 //--------------------------------------------------------------
 void ofApp::setup(){
 	// listen on the given port
 	cout << "listening for osc messages on port " << PORT << "\n";
 	receiver.setup(PORT);
-
-	current_msg_string = 0;
-	mouseX = 0;
-	mouseY = 0;
-	mouseButtonState = "";
 
 	ofBackground(30, 30, 130);
 
@@ -18,31 +14,28 @@ void ofApp::setup(){
 //--------------------------------------------------------------
 void ofApp::update(){
 
-	// hide old messages
-	for(int i = 0; i < NUM_MSG_STRINGS; i++){
-		if(timers[i] < ofGetElapsedTimef()){
-			msg_strings[i] = "";
-		}
-	}
-
 	// check for waiting messages
 	while(receiver.hasWaitingMessages()){
 		// get the next message
 		ofxOscMessage m;
 		receiver.getNextMessage(&m);
 //        cout<<m.getAddress()<<endl;
-        if(m.getAddress() == "/motion/detectionStatus")
-            detectionStatus=m.getArgAsString(0); cout<<detectionStatus<<endl;
+        if(m.getAddress() == (OSC_KINECT_ADDR_BASE+OSC_KINECT_ADDR_STATE))
+            detectionStatus = m.getArgAsString(0);
+        if(m.getAddress() == (OSC_KINECT_ADDR_BASE+OSC_KINECT_ADDR_STATE+OSC_KINECT_STATE_POSITIONED))
+            userPositioned = m.getArgAsBool(0);
         if(m.getAddress() == "/motion/leftHand/position")
-            handL=ofPoint(m.getArgAsFloat(0), m.getArgAsFloat(1));
+            handL = ofPoint(m.getArgAsFloat(0), m.getArgAsFloat(1));
         if(m.getAddress() == "/motion/rightHand/position")
-            handR=ofPoint(m.getArgAsFloat(0), m.getArgAsFloat(1));
+            handR = ofPoint(m.getArgAsFloat(0), m.getArgAsFloat(1));
 	}
 }
 
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+    
+    
     
     if(detectionStatus=="Capturing"){
         ofPushStyle();
@@ -53,28 +46,6 @@ void ofApp::draw(){
         ofDrawCircle(handR.x * ofGetWidth(), handR.y * ofGetHeight(), 20);
         ofPopStyle();
     }
-
-    
-//	string buf;
-//	buf = "listening for osc messages on port" + ofToString(PORT);
-//	ofDrawBitmapString(buf, 10, 20);
-//    
-//    if(receivedImage.getWidth() > 0){
-//        ofDrawBitmapString("Image:", 10, 160);
-//        receivedImage.draw(10, 180);
-//    }
-//
-//	// draw mouse state
-//	buf = "mouse: " + ofToString(mouseX, 4) +  " " + ofToString(mouseY, 4);
-//	ofDrawBitmapString(buf, 430, 20);
-//	ofDrawBitmapString(mouseButtonState, 580, 20);
-//
-//	for(int i = 0; i < NUM_MSG_STRINGS; i++){
-//		ofDrawBitmapString(msg_strings[i], 10, 40 + 15 * i);
-//	}
-//
-    
-
 }
 
 //--------------------------------------------------------------
